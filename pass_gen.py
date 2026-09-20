474e5ebaae5a4a9ebea7789c6964a5ce2a0f5b8d
clmmcdc <chuongbinnguyen@gmail.com>
2026-09-20 10:08:06 +0700

    v2

--- pass_gen.py
+++ pass_gen.py
@@ -3,7 +3,6 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 numbers = "0123456789"
 symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?/"
 ALPHABET = alphabet.upper()
-print("Welcome to the Password Generator!")
-input_length = int(input("Enter the desired length of your password: "))
-password = ''.join(random.choice(alphabet + ALPHABET + numbers + symbols) for _ in range(input_length))
-print("Your generated password is:", password)
+p_len = int(input("Enter the length of the password: "))
+password = ''.join(random.choice(alphabet + ALPHABET + numbers + symbols) for _ in range(p_len))
+print("Your password is:", password)
